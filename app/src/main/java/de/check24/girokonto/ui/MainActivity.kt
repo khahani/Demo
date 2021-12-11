e11771a04675543d93a60593a6947c23b8b31ced
Mohammadreza Khahani <mr.khahani@gmail.com>
2021-12-11 14:00:01 +0330

    Move MainActivity.kt to ui package

--- app/src/main/java/de/check24/girokonto/ui/MainActivity.kt
+++ app/src/main/java/de/check24/girokonto/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package de.check24.girokonto
+package de.check24.girokonto.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import de.check24.girokonto.R
 import de.check24.girokonto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
